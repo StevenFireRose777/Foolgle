d642602b8afc95ea3d37f818e08ad928e8d01d62
StevenFireRose777 <stevennnuno769@gmail.com>
2025-10-06 09:51:31 -0700

    Added timer functionality to event from Surprise-button

--- google-clone/surprise.js
+++ google-clone/surprise.js
@@ -1,6 +1,13 @@
-document
-  .querySelector(".surprise-button")
+
+
+function makeInvisable(){
+  document.getElementById("lizard-dance").style.opacity = 0;
+}
+
+document.querySelector(".surprise-button")
   .addEventListener("click", function () {
-    console.log("clicked!");
+    // console.log("clicked!");
     document.getElementById("lizard-dance").style.opacity = 1;
+    setTimeout(makeInvisable, 10000); // 10 seconds
+
   });
