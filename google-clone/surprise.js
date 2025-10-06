863fce90ac65dccc1959c06226496bb572662c41
StevenFireRose777 <stevennnuno769@gmail.com>
2025-10-06 10:02:33 -0700

    Made sure timer worked

--- google-clone/surprise.js
+++ google-clone/surprise.js
@@ -4,10 +4,16 @@ function makeInvisable(){
   document.getElementById("lizard-dance").style.opacity = 0;
 }
 
+let timerID;
+
 document.querySelector(".surprise-button")
   .addEventListener("click", function () {
+
+    if(timerID){
+      clearTimeout(timerID);
+    }
     // console.log("clicked!");
     document.getElementById("lizard-dance").style.opacity = 1;
-    setTimeout(makeInvisable, 10000); // 10 seconds
+    timerID = setTimeout(makeInvisable, 10000); // 10 seconds
 
   });
