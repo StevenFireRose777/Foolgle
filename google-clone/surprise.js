63e24b492260ba9a7ff83f8928b8ec2338280e69
StevenFireRose777 <stevennnuno769@gmail.com>
2025-10-06 09:37:38 -0700

    Added JS file for surprise button functionality.

---
+++ google-clone/surprise.js
@@ -0,0 +1,6 @@
+document
+  .querySelector(".surprise-button")
+  .addEventListener("click", function () {
+    console.log("clicked!");
+    document.getElementById("lizard-dance").style.opacity = 1;
+  });
